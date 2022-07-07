68f68b97d096c8536dca8110f3a1d1908294033b
donghyum <dong60947@gmail.com>
2022-07-08 00:29:44 +0900

    생성된 게시물 정보를 Article 클래스의 객체로 저장 후 문장으로 출력

--- src/com/ldh/exam/board/Main.java
+++ src/com/ldh/exam/board/Main.java
@@ -26,7 +26,13 @@ public class Main {
         String body = sc.nextLine();
         int id = articleLastId + 1;
         articleLastId++;
-        System.out.printf("%d번째 게시물이 등록 되었습니다.\n", id);
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번째 게시물이 등록 되었습니다.\n", article.id);
       }
       else {
         System.out.printf("입력 된 명령어 : %s\n", cmd);
@@ -37,4 +43,10 @@ public class Main {
 
     sc.close();
   }
+}
+
+class Article{
+  int id;
+  String title;
+  String body;
 }
