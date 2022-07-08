8ed66ad9a114d2fae4da2957cc4217de6f14a19f
donghyum <dong60947@gmail.com>
2022-07-08 23:40:07 +0900

    /usr/article/detail 이라고 입력하면, 가장 마지막에 입력받은 게시물의 상세정보 출력

--- src/com/ldh/exam/board/Main.java
+++ src/com/ldh/exam/board/Main.java
@@ -9,6 +9,7 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
 
     int articleLastId = 0;
+    Article lastArticle = null;
 
     while (true) {
       System.out.printf("명령) ");
@@ -18,19 +19,36 @@ public class Main {
         break;
       }
 
-      else if (cmd.equals("/user/article/write")){
-        System.out.println("게시물 출력");
+      else if(cmd.equals("/usr/article/detail")) {
+
+        if(lastArticle == null){
+          System.out.println("게시물이 없습니다!");
+          continue;
+        }
+
+        Article article = lastArticle;
+
+        System.out.println("- 게시물 상세보기 -");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+
+
+
+
+      }
+      else if( cmd.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
-        int id = articleLastId + 1;
-        articleLastId++;
+        int id = ++articleLastId;
 
         Article article = new Article(id, title, body);
-
+        lastArticle = article;
         System.out.println("생성된 게시물 객체 : " + article);
-        System.out.printf("%d번째 게시물이 등록 되었습니다.\n", article.id);
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
       }
       else {
         System.out.printf("입력 된 명령어 : %s\n", cmd);
@@ -48,7 +66,7 @@ class Article {
   String title;
   String body;
 
-  Article(int id_, String title_, String body_){
+  Article(int id_, String title_, String body_) {
     this.id = id_;
     this.title = title_;
     this.body = body_;
@@ -56,6 +74,6 @@ class Article {
 
   @Override
   public String toString() {
-    return String.format("{id: %d, title: %s, body: %s}", id, title, body);
+    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
   }
 }
